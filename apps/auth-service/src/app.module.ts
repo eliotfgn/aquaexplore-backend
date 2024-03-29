2a5212b48242656dcd8cc00b7dec9fe22c5b8568
Eliot Fagnon <efagnon2002@gmail.com>
2024-03-29 01:29:40 +0100

    feat: Add Auth module

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
+import { AuthModule } from './core/features/auth/auth.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
