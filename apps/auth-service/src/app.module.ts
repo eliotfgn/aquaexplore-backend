44098aa220dfba2fd33cae3d5c8567649aacb78e
Eliot Fagnon <efagnon2002@gmail.com>
2024-03-29 01:31:04 +0100

    feat: Add user module

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 import { AuthModule } from './core/features/auth/auth.module';
+import { UserModule } from './core/features/user/user.module';
 
 @Module({
-  imports: [DbModule, AuthModule],
+  imports: [DbModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService]
 })
