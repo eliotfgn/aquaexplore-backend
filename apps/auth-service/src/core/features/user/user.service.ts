6edcfe8441842564266efc2afb68b7c2390ea5ed
Eliot Fagnon <efagnon2002@gmail.com>
2024-04-02 00:32:56 +0100

    feat: Inject db client in user service

--- apps/auth-service/src/core/features/user/user.service.ts
+++ apps/auth-service/src/core/features/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DB_CLIENT } from 'src/db/db.provider';
+import DbClient from 'src/db/db.type';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@Inject(DB_CLIENT) private readonly dbClient: DbClient) {}
+}
