c81f5b9c13dc27b40f312f4407e872ee12d5a50d
Eliot Fagnon <efagnon2002@gmail.com>
2024-04-02 23:48:36 +0100

    feat: Register user

--- apps/auth-service/src/core/features/auth/auth.service.ts
+++ apps/auth-service/src/core/features/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DB_CLIENT } from '@db/db.provider';
+import DbClient from '@db/db.type';
+import { CreateUserDto } from '@aquaexplore/types';
+import { hashPassword } from '@/utils/password.util';
+import { UserService } from '@features/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(DB_CLIENT) private readonly dbClient: DbClient,
+    private readonly userService: UserService
+  ) {}
+
+  async register(payload: CreateUserDto) {
+    const hashedPassword = await hashPassword(payload.password);
+    payload.password = hashedPassword;
+
+    const user = await this.userService.createUser(payload);
+
+    //TODO: Trigger account verification process
+    //TODO: Send account creation notification
+
+    return user;
+  }
+}
--- apps/auth-service/src/core/features/user/user.service.ts
+++ apps/auth-service/src/core/features/user/user.service.ts
@@ -1,5 +1,5 @@
 import DbClient from '@db/db.type';
-import { CreateUserDto } from '@aquaexplore/types';
+import { CreateUserDto, UserEntity } from '@aquaexplore/types';
 import { Inject, Injectable } from '@nestjs/common';
 import { DB_CLIENT } from '@db/db.provider';
 import { users } from '@db/schema';
@@ -8,12 +8,12 @@ import { users } from '@db/schema';
 export class UserService {
   constructor(@Inject(DB_CLIENT) private readonly dbClient: DbClient) {}
 
-  async createUser(payload: CreateUserDto): Promise<CreateUserDto> {
+  async createUser(payload: CreateUserDto): Promise<UserEntity> {
     const query: CreateUserDto[] = await this.dbClient
       .insert(users)
       .values(payload)
       .returning();
-    const user: CreateUserDto = query[0];
+    const user: UserEntity = query[0];
 
     return user;
   }
