e313189a040cd28d4ac26862b10fd17e64aeac69
Eliot Fagnon <efagnon2002@gmail.com>
2024-04-02 01:42:51 +0100

    feat: Add user entity

---
+++ packages/types/auth/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import { IsBoolean, IsDate, IsEmail, IsEnum, IsString, IsUUID, Min } from 'class-validator';
+
+enum Role {
+    USER = 'user',
+    ADMIN = 'admin',
+    EXPERT = 'expert'
+}
+
+export class UserEntity {
+    @IsUUID()
+    id!: string;
+
+    @IsString()
+    firstName?: string;
+
+    @IsString()
+    lastName?: string;
+
+    @IsString()
+    @IsEmail()
+    email!: string;
+
+    @IsString()
+    @Min(8)
+    password!: string;
+
+    @IsEnum(Role)
+    role!: Role;
+
+    @IsBoolean()
+    isVerified!: boolean;
+
+    @IsDate()
+    createdAt!: Date;
+
+    @IsDate()
+    updatedAt!: Date;
+}
