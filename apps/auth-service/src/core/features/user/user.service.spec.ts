43b4ad1838974a1e26bde88c43af8fdc7fec3e97
Eliot Fagnon <efagnon2002@gmail.com>
2024-04-03 20:36:25 +0100

    test: Config db client mock for user service

--- apps/auth-service/src/core/features/user/user.service.spec.ts
+++ apps/auth-service/src/core/features/user/user.service.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { mock, mockReset } from 'jest-mock-extended';
+import DbClient from '@/core/db/db.type';
+import { DB_CLIENT } from '@/core/db/db.provider';
 
 describe('UserService', () => {
+  const mockDbClient = mock<DbClient>();
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService]
+      providers: [UserService, { provide: DB_CLIENT, useValue: mockDbClient }]
     }).compile();
 
     service = module.get<UserService>(UserService);
+  });
+
+  beforeEach(() => {
+    mockReset(mockDbClient);
   });
 
   it('should be defined', () => {
