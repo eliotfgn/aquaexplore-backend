1fed566d6b192bb494db5b97bff5511fad252cfc
Eliot Fagnon <efagnon2002@gmail.com>
2024-04-23 03:50:38 +0100

    feat: Add token module

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DbModule } from '@db/db.module';
 import { AuthModule } from '@features/auth/auth.module';
 import { UserModule } from '@features/user/user.module';
+import { TokenModule } from './core/shared/token/token.module';
 
 @Module({
-  imports: [DbModule, AuthModule, UserModule],
+  imports: [DbModule, AuthModule, UserModule, TokenModule],
   controllers: [AppController],
   providers: [AppService]
 })
