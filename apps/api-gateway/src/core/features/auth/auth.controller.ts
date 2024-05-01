3a5eaa4c2b7196c1b6060ef9e92c4c915faf4f13
Eliot Fagnon <efagnon2002@gmail.com>
2024-05-01 01:56:38 +0100

    feat(api-gateway): setup kafka client for auth service

--- apps/api-gateway/src/core/features/auth/auth.controller.ts
+++ apps/api-gateway/src/core/features/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, OnModuleInit } from '@nestjs/common';
+import { Client, ClientKafka } from '@nestjs/microservices';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController implements OnModuleInit {
+  @Client({
+    options: {
+      client: {
+        clientId: 'api-gateway',
+        brokers: [process.env.KAFKA_BROKER],
+      },
+      consumer: {
+        groupId: 'aqua-explore/api-gateway',
+      },
+    },
+  })
+  private readonly authServiceClient: ClientKafka;
+
+  constructor(private readonly authSerive: AuthService) {}
+
+  async onModuleInit() {
+    this.authServiceClient.subscribeToResponseOf('login');
+    await this.authServiceClient.connect();
+  }
+}
