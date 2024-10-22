f2479714128d5f0d08afb5754764a7fa6f836c65
Eliot Fagnon <efagnon2002@gmail.com>
2024-10-22 15:17:27 +0100

    build: Configure turborepo

--- apps/auth-service/src/core/features/auth/auth.service.spec.ts
+++ apps/auth-service/src/core/features/auth/auth.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { CreateUserDto } from '@aquaexplore/types';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const userServiceMock: DeepMockProxy<UserService> = mockDeep<UserService>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService]
+      providers: [
+        AuthService,
+        { provide: UserService, useValue: userServiceMock }
+      ]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +21,11 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Registration', () => {
+    it('Should throw error when email already exists', async () => {
+      //Given
+    });
   });
 });
--- apps/auth-service/src/core/features/auth/verification/verification.entity.ts
+++ apps/auth-service/src/core/features/auth/verification/verification.entity.ts
@@ -1,3 +1,1 @@
-export class VerificationCode {
-    
-}
+export class VerificationCode {}
